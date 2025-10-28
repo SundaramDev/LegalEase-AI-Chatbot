a132dec06d69b16e2e8e66763155a975e62a9d67
SundaramDev <sundaramkumar75655@gmail.com>
2025-10-29 01:15:12 +0530

    Vector database added

--- frontend.py
+++ frontend.py
@@ -8,5 +8,15 @@ uploaded_file = st.file_uploader("Upload PDF",
 
 
 # step2: Chatbot Skeleton (Questions & answer)
-user_query = st.text_area("Enter your prompt: ", height=150, placeholder="Aks Anything!")
+user_query = st.text_area("Enter your prompt: ", height=150, placeholder="Ask Anything!")
 
+ask_question = st.button("Ask AI Lawyer")
+
+if ask_question:
+    if uploaded_file:
+        st.chat_message("user").write(user_query)
+    fixed_response = "Hi, this is a fixed response"
+    st.chat_message("AI Lawyer").write(fixed_response)
+
+else :
+    st.error("Kindly upload a valid PDF file first ! ")    
